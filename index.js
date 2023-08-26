12e492216fc177ac89c3af2dd0d9218cd2ea9bf9
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-26 17:03:59 -0300

    Module of routers added in index

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const router = require("./routers/index");
+
+router(app);
 
 app.listen(port, (error) => {
     if(error){
