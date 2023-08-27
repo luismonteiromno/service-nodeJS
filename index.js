46db4af4f697e8fdd2ddfc6d6935094efc35f19c
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-26 22:00:49 -0300

    Connection added in index

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const router = require("./routers/index");
+const conection = require("./database/data");
+const tables = require("./database/tables");
 
-router(app);
+
+router(app, express);
+tables.init(conection);
+
 
 app.listen(port, (error) => {
     if(error){
