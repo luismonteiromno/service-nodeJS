a6448eee4c6e80cc53e1bced84bc798c1eb74015
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-26 22:44:31 -0300

    Services finished pt2

--- models/servicesModels.js
+++ models/servicesModels.js
@@ -1,59 +1,34 @@
 const conection  = require("../database/data");
+
 class servicesModels {
-    list() {
-        const sql = "SELECT * FROM services";
+    execute_query(sql, params = "") {
         return new Promise((resolve, reject) => {
-            conection.query(sql, {}, (error, response) => {
+            conection.query(sql, params, (error, response) => {
                 if (error){
-                    console.log(`Error ${error} na listagem`);
                     reject(error);
                 }
-                console.log("Sucesso");
                 resolve(response);
             })
         })
+    }
+    list() {
+        const sql = "SELECT * FROM services";
+        this.execute_query(sql);
     }
 
     create(newService) {
         const sql = "INSERT INTO services SET ?";
-        return new Promise((resolve, reject) => {
-            conection.query(sql, newService, (error, response) => {
-                if (error){
-                    console.log(`Error ${error} na criação`);
-                    reject(error);
-                }
-                console.log("Sucesso");
-                resolve(response);
-            })
-        })
+        this.execute_query(sql, newService);
     }
 
     update(updateService, id) {
         const sql = "UPDATE services SET ? WHERE id = ?";
-        return new Promise((resolve, reject) => {
-            conection.query(sql, [updateService, id], (error, response) => {
-                if (error){
-                    console.log(`Error ${error} na atualização`);
-                    reject(error);
-                }
-                console.log("Sucesso");
-                resolve(response);
-            })
-        })
+        this.execute_query(sql, [updateService, id]);
     }
 
     delete(id) {
         const sql = "DELETE FROM services WHERE id = ?";
-        return new Promise((resolve, reject) => {
-            conection.query(sql, id, (error, response) => {
-                if (error){
-                    console.log(`Error ${error} na hora de deletar`);
-                    reject(error);
-                }
-                console.log("Sucesso");
-                resolve(response);
-            })
-        })
+        this.execute_query(sql, id);
     }
 }
 
