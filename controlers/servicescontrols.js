781f7d4e486f0da368e30b70a768ba61f0b1788f
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-26 22:44:25 -0300

    Services finished pt1

--- controlers/servicescontrols.js
+++ controlers/servicescontrols.js
@@ -1,16 +1,33 @@
 const servicesModels = require("../models/servicesModels");
+
 class ServicesControler {
-    search() {
-        return servicesModels.list();
+    search(req, res) {
+        const listServices  = servicesModels.list();
+        return listServices
+        .then((services) => res.status(200).json(services))
+        .catch((error) => res.status(404).json(error.message));
     }
-    create(newService) {
-        return servicesModels.create(newService);
+    create(req, res) {
+        const newService = req.body;
+        const create = servicesModels.create(newService);
+        return create
+        .then((serviceCreate) => res.status(201).json(serviceCreate))
+        .catch((error) => res.status(400).json(error.message));
     }
-    update(updateService, id) {
-        return servicesModels.update(updateService, id);
+    update(req, res) {
+        const { id } = req.params;
+        const serviceUpdate = req.body;
+        const update = servicesModels.update(serviceUpdate, id);
+        update
+        .then((resultService) => res.status(200).json(resultService))
+        .catch((error) => res.status(400).json(error.message));
     }
-    delete(id) {
-        return servicesModels.delete(id);
+    delete(req, res) {
+        const { id } = req.params;
+        const deleteService = servicesModels.delete(id);
+        return deleteService
+        .then((deleteService) => res.status(200).json(deleteService))
+        .catch((error) => res.status(400).json(error.message));
     }
 }
 
