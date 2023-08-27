90e8b9d4673984a7219a0a906cd1429019331c4b
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-26 22:01:06 -0300

    Services of routers created

--- routers/servicesRoutes.js
+++ routers/servicesRoutes.js
@@ -1,22 +1,35 @@
 const { Router } = require("express");
 const router = Router();
+const servicesControler = require("../controlers/servicescontrols");
 
 router.get('/services', (req, res) => {
-    res.send("Listando todos os atendimentos encontrados");
+    const listServices  = servicesControler.search();
+    listServices
+        .then((services) => res.status(200).json(services))
+        .catch((error) => res.status(404).json(error.message));
 });
 
 router.post('/services', (req, res) => {
-    res.send("Novo atendimento solicitado");
+    const newService = req.body;
+    const create = servicesControler.create(newService);
+    create
+        .then((serviceCreate) => res.status(201).json(serviceCreate))
+        .catch((error) => res.status(400).json(error.message));
 });
 
 router.put('/service/:id', (req, res) => {
     const { id } = req.params;
-    res.send(`Atendimento ${id} atualizado`);
+    const serviceUpdate = req.body;
+    const update = servicesControler.update(serviceUpdate, id);
+    update
+        .then((resultServices) => res.status(201).json(resultServices))
+        .catch((error) => res.status(400).json(error.message));
 });
 
 router.get('/service/:id', (req, res) => {
     const { id } = req.params;
-    res.send(`Atendimento ${id} deletado`);
+    const response = servicesControler.delete(id);
+    res.send(response);
 });
 
 module.exports = router;
