a732c48a4a73307e0bdf72f7274bc265ea43ebe0
Luis Monteiro <felipemoreno9065@gmail.com>
2023-08-26 22:01:34 -0300

    Models created

---
+++ models/servicesModels.js
@@ -0,0 +1,46 @@
+const conection  = require("../database/data");
+class servicesModels {
+    list() {
+        const sql = "SELECT * FROM services";
+        return new Promise((resolve, reject) => {
+            conection.query(sql, {}, (error, response) => {
+                if (error){
+                    console.log(`Error ${error} na listagem`);
+                    reject(error);
+                }
+                console.log("Sucesso");
+                resolve(response);
+            })
+        })
+    }
+
+    create(newService) {
+        const sql = "INSERT INTO services SET ?";
+        return new Promise((resolve, reject) => {
+            conection.query(sql, newService, (error, response) => {
+                if (error){
+                    console.log(`Error ${error} na criação`);
+                    reject(error);
+                }
+                console.log("Sucesso");
+                resolve(response);
+            })
+        })
+    }
+
+    update(updateService, id) {
+        const sql = "UPDATE services SET ? WHERE id = ?";
+        return new Promise((resolve, reject) => {
+            conection.query(sql, [updateService, id], (error, response) => {
+                if (error){
+                    console.log(`Error ${error} na criação`);
+                    reject(error);
+                }
+                console.log("Sucesso");
+                resolve(response);
+            })
+        })
+    }
+}
+
+module.exports = new servicesModels();
